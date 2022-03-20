1be8a94181a5425ef08ffc2afde12254d2388cd6
rodrigmatrix <rodrigmatrix@gmail.com>
2022-03-20 17:48:37 -0300

    added icons for tv and wearos

--- weather-you/tv/src/main/java/com/rodrigmatrix/weatheryou/tv/presentation/theme/Theme.kt
+++ weather-you/tv/src/main/java/com/rodrigmatrix/weatheryou/tv/presentation/theme/Theme.kt
@@ -72,7 +72,7 @@ private val DarkColorScheme = darkColorScheme(
 
 @Composable
 fun WeatherYouTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = true,
     dynamicColor: Boolean = Build.VERSION.SDK_INT >= 31,
     content: @Composable () -> Unit
 ) {
